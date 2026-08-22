e474f6988ba5319634e7a4377c729c5c748d3b93
AngelinaSatchi <angelinasashixinga@gmail.com>
2026-08-23 00:56:11 +0200

    [S5/L16]

--- src/app/service/dragonball.service.ts
+++ src/app/service/dragonball.service.ts
@@ -1,18 +1,27 @@
 import { effect, Injectable, signal } from "@angular/core"
 import { Character } from "../interfaces/character.interface";
 
+const  loadFromLocalStorage = (): Character[] => {
+
+  const characters = localStorage.getItem('characters');
+return characters ? JSON.parse(characters) :[];
+};
+
+
+
 @Injectable({providedIn: 'root'})
 export class DrangonballService  {
+  characters = signal<Character[]>( loadFromLocalStorage());
     
 
-     characters = signal<Character[]>([
+    /*  characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
     { id: 2, name: 'Vegeta', power: 8000 },
   ]);
-
+ */
    saveToLocalStorage = effect( () => {
     /* console.log('Character count ${this.characters.length}'); */
-    localStorage.setItem('character', JSON.stringify(this.characters()));
+    localStorage.setItem('characters', JSON.stringify(this.characters()));
    })
 
 addCharacter(character: Character) {
