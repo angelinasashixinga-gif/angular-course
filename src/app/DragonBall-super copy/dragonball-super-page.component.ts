cdec5b6882a3b838fc49b54f75416f4ba33f64e6
AngelinaSatchi <angelinasashixinga@gmail.com>
2026-08-24 09:17:45 +0100

    S5/l17

--- src/app/DragonBall-super copy/dragonball-super-page.component.ts
+++ src/app/DragonBall-super copy/dragonball-super-page.component.ts
@@ -19,6 +19,42 @@ export class dragonballSuperPageComponent {
 
 public dragonballService = inject (DrangonballService);
 
+  name = signal('');
+  power = signal(0);
 
+  characters = signal<Character[]>([
+    { id: 1, name: 'Goku', power: 9001 },
+    { id: 2, name: 'Vegeta', power: 8000 },
+    /* { id: 4, name: 'Yamcha', power: 500 },
+    { id: 3, name: 'Piccolo', power: 3000 }, */
+
+    
+  ]);
+
+
+addCharacter() {
+
+  if (!this.name() || !this.power() || this.power() <=  6) {
+    return;
+  }
+  const newCharacter:Character = {
+    id: this.characters().length + 1,
+    name: this.name(),
+    power: this.power(),
+  };
+
+  this.characters.update(
+    (list) => [...list, newCharacter]);
+    this.resetFields();
 
 }
+resetFields() {
+  this.name.set('');
+  this.power.set(0);
+}
+
+}
+
+
+
+
